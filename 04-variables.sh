9d1a0994bf68a3917361cda1ab16623b8cf1bf73
Sreenu <srinivasuluchilakori@gmail.com>
2024-08-30 12:39:18 +0530

    added 2nd script

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, good morning"
+echo "$PERSON2: Hello $PERSON1, very good morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, How about you?"
+
