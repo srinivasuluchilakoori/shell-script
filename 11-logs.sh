1bad3deb247b14520320fa7e9f16c1217e75d725
Sreenu <srinivasuluchilakori@gmail.com>
2024-08-31 12:23:48 +0530

    another script added

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,24 +4,28 @@ ID=$(id -u)
 
 TIMESTAMP=$(date +%F---%H-%M-%S)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 .... failed"
+        echo "$R ERROR:: $2 .... failed $N"
         exit 1
     else
-        echo "$2 .... success"
+        echo "$G $2 .... success $N"
     fi
 }
 
 if  [ $ID -ne 0 ]
 then
-    echo "ERROR:: Please run the script with root access"
+    echo "ERROR:: $R Please run the script with root access $N"
     exit 1
 else
-    echo "you are root user"
+    echo "$G you are root user $N"
 fi
 
 yum install mysql -y &>> $LOGFILE
