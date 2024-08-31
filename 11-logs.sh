65072d42311769fb3d8fdf7d12c68c6653ae6b98
Sreenu <srinivasuluchilakori@gmail.com>
2024-08-31 12:33:25 +0530

    another script added

--- 11-logs.sh
+++ 11-logs.sh
@@ -13,10 +13,10 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$R ERROR:: $2 .... failed $N"
+        echo -e "ERROR:: $2 .... $R failed $N"
         exit 1
     else
-        echo -e "$G $2 .... success $N"
+        echo -e "$2 .... $G  success $N"
     fi
 }
 
