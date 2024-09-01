58c69d4db9f12d146dc4c20b7de415b972ab2ae5
Sreenu <srinivasuluchilakori@gmail.com>
2024-09-01 11:44:30 +0530

    another script added

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,6 +6,15 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
 if  [ $ID -ne 0 ]
 then
     echo -e "ERROR:: $R Please run the script with root access $N"
@@ -15,4 +24,13 @@ else
 fi
 
 echo "All arguments are passed: $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "installation of $package"
+done
 
